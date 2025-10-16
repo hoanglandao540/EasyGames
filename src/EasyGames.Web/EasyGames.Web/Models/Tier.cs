4ebc576dbabbe92c0fc91975bb96f13a9668b5f0
Hoang Lan Dao <hoanglandao540@gmail.com>
2025-10-16 23:07:58 +0600

    feat(service): add Tier enum + TierService with basic threshold tests

---
+++ src/EasyGames.Web/EasyGames.Web/Models/Tier.cs
@@ -0,0 +1,14 @@
+﻿// Simple customer tier levels based on lifetime spending.
+
+namespace EasyGames.Web.Models
+{
+    public enum Tier
+    {
+        Bronze = 0, // default / starter tier
+        Silver = 1,
+        Gold = 2
+    }
+}
+
+
+
