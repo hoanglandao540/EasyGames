afb112d9568cf9065e0b8a228ba89ce70be70a7f
Hoang Lan Dao <hoanglandao540@gmail.com>
2025-10-17 13:15:36 +0600

    no message

--- src/EasyGames.Web/EasyGames.Web/Models/OrderLine.cs
+++ src/EasyGames.Web/EasyGames.Web/Models/OrderLine.cs
@@ -3,13 +3,16 @@
     public class OrderLine
     {
         public int Id { get; set; }
-        public int OrderId { get; set; }     // FK by convention
+        public int OrderId { get; set; }      // FK by convention
         public int ProductId { get; set; }
+
         public string Name { get; set; } = "";
         public decimal Price { get; set; }
         public int Qty { get; set; }
-        public decimal LineTotal => Price * Qty;
+
+        public Order? Order { get; set; }     // nav back to Order
     }
 }
+
 
 
